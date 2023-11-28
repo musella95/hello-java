877c9289405c36f67fcfe49305cb12296e818893
musella95 <musellamatteoo@hotmail.com>
2023-11-28 15:45:39 +0100

    add Mondo for prof

--- src/org/lesson/java/StampaSaluto.java
+++ src/org/lesson/java/StampaSaluto.java
@@ -2,6 +2,6 @@ package org.lesson.java;
 
 public class StampaSaluto {
     public static void main(String[] args) {
-        System.out.println("Ciao");
+        System.out.println("Ciao Mondo");
     }
 }
